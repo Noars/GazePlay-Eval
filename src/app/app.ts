45e4575e649d0008fd888012d6ef8028fe294988
Noars <55838787+Noars@users.noreply.github.com>
2025-07-16 16:16:19 +0200

    check f5 reload

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {filter} from 'rxjs';
@@ -11,7 +11,7 @@ import {ProgressBarComponent} from './components/progress-bar/progress-bar.compo
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App{
+export class App implements OnInit{
   protected title = 'GazePlay-Eval';
 
   steps = ['Informations Evaluation', 'Informations Participant', 'Modes et Paramètres', 'Création et Modifications', 'Téléchargement et avis'];
@@ -37,5 +37,14 @@ export class App{
         this.currentStepIndex = -1;
       }
     });
+  }
+
+  ngOnInit(): void {
+    const nav = performance.getEntriesByType('navigation')[0] as PerformanceNavigationTiming;
+    const isReload = nav?.type === 'reload';
+
+    if (isReload && this.router.url !== '/home') {
+      this.router.navigate(['/home']);
+    }
   }
 }
--- src/app/components/eval-manual/eval-manual.component.ts
+++ src/app/components/eval-manual/eval-manual.component.ts
@@ -2,13 +2,12 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {Router} from '@angular/router';
-import {MatTooltip} from '@angular/material/tooltip';
 import {SaveService} from '../../services/save/save.service';
 import {evalModeModel, saveModelDefault} from '../../shared/saveModel';
 
 @Component({
   selector: 'app-eval-manual',
-  imports: [CommonModule, FormsModule, MatTooltip],
+  imports: [CommonModule, FormsModule],
   templateUrl: './eval-manual.component.html',
   styleUrl: './eval-manual.component.css'
 })
@@ -17,18 +16,8 @@ export class EvalManualComponent implements OnInit{
   @Input() selectedMode: evalModeModel = null;
   @Output() selectedModeChange = new EventEmitter<null>();
 
-  useGlobalParams: boolean = false;
-
-  /* ----- Ecran transition -----*/
-  transitionTypes: string[] = ['Écran noir', 'Écran de fixation', 'Écran d’attente', 'Écran de pause'];
-  transitionInfos: string[] = ['1', '2', '3', '4'];
-  selectedTransition = this.transitionTypes[0];
-  navigationMethods: string[] = ['Clique souris / clavier', 'Temps'];
-  selectedMethod = this.navigationMethods[0];
-
-  /* ----- Ecran stimuli -----*/
   // 0-Nb rows, 1-Nb cols, 2-Add max time screen, 3-Max time screen, 4-Fixation length, 5-Nb stimuli, 6-Disable stimuli, 7-Random position stimuli
-  screenStimuliInfos: string[] = saveModelDefault.globalParamsStimuli;
+  globalStimuliInfos: string[] = saveModelDefault.globalParamsStimuli;
 
   constructor(private router: Router,
               private saveService: SaveService,) {
@@ -39,19 +28,16 @@ export class EvalManualComponent implements OnInit{
   }
 
   loadData(){
-    this.screenStimuliInfos = this.saveService.dataAuto.globalParamsStimuli;
-  }
-
-  getInfoTransition(): string {
-    const idx = this.transitionTypes.indexOf(this.selectedTransition);
-    return this.transitionInfos[idx] ?? '';
+    this.globalStimuliInfos = this.saveService.dataAuto.globalParamsStimuli;
   }
 
   startCreateEvalManual(){
-    //this.router.navigate(['/create-eval']);
+    this.saveService.saveDataAuto(this.saveService.dataAuto.nomEval, this.saveService.dataAuto.format, this.saveService.dataAuto.infoParticipant, this.globalStimuliInfos);
+    this.router.navigate(['/create-eval']);
   }
 
   backToEvalChoice(){
+    this.saveService.saveDataAuto(this.saveService.dataAuto.nomEval, this.saveService.dataAuto.format, this.saveService.dataAuto.infoParticipant, this.globalStimuliInfos);
     this.selectedModeChange.emit(null);
   }
 
