7b61e7425794cc1406862bb275dbd2fde1c01a27
Noars <55838787+Noars@users.noreply.github.com>
2025-07-16 17:10:27 +0200

    start eval creation

--- src/app/pages/create-eval/create-eval.component.ts
+++ src/app/pages/create-eval/create-eval.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-create-eval',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-eval.component.html',
   styleUrl: './create-eval.component.css'
 })
 export class CreateEvalComponent {
 
-  constructor() {
+  constructor(private router: Router,) {
+  }
+
+  backToSetupEval() {
+    this.router.navigate(['/setup-eval']);
+  }
+
+  goToDownloadEval() {
+    //this.router.navigate(['/download-eval']);
   }
 }
