61502ea0f15b4654b9cc5aa68c999a851aebfe41
FYRAXYS <fyraxys2006@gmail.com>
2026-01-10 16:56:35 +0100

    Suppression du code inutile
    Mise en forme et nettoyage du code pour ne garder que les imports et les méthodes utilisés.
    Vu que cette nouvelle page ne sauvergarde rien, la logique d'enregistrement n'est plus nécessaire.

--- src/app/pages/newpage/newpage.component.ts
+++ src/app/pages/newpage/newpage.component.ts
@@ -1,56 +1,23 @@
-import {Component, OnInit} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {CommonModule} from '@angular/common';
-import {SaveService} from '../../services/save/save.service';
-import {formatTypeModel, saveModelDefault} from '../../shared/saveModel';
+import {Component} from '@angular/core';
 import {Router} from '@angular/router';
-import {MatTooltip} from '@angular/material/tooltip';
-
 
 @Component({
   selector: 'app-newpage-eval',
-  imports: [CommonModule, FormsModule, MatTooltip],
+  imports: [],
   templateUrl: './newpage.component.html',
   styleUrl: './newpage.component.css'
 })
-export class NewpageComponent implements OnInit{
-  evaluationName: string = saveModelDefault.nomEval;
-  resultType: formatTypeModel = saveModelDefault.format;
-  tooltipEvalName: string = 'Ce nom apparaîtra dans le nom des fichiers de résultats, ainsi que dans Gazeplay afin que vous puissiez sélectionner cette évaluation parmi celles que vous aurez déjà créées';
-  tooltipEvalType: string = 'Les résultats enregistrés en XLSX et CSV seront lisibles dans un tableur';
-
+export class NewpageComponent{
   dateHeure: string = ''; // String pour l'affichage de la date et de l'heure
 
-  constructor(private router: Router,private saveService: SaveService) {
-  }
-
-  ngOnInit(): void {
-    this.loadData()
-  }
-
-  saveData(){
-    this.saveService.saveDataAuto(
-      this.evaluationName,
-      this.resultType,
-      this.saveService.dataAuto.infoParticipant,
-      this.saveService.dataAuto.globalParamsTransitionScreen,
-      this.saveService.dataAuto.globalParamsInstructionScreen,
-      this.saveService.dataAuto.globalParamsStimuliScreen,
-      this.saveService.dataAuto.listScreens);
-  }
-
-  loadData(){
-    this.evaluationName = this.saveService.dataAuto.nomEval;
-    this.resultType = this.saveService.dataAuto.format;
+  constructor(private router: Router) {
   }
 
   backToInfoEval() {
-    this.saveData();
     this.router.navigate(['/info-eval']);
   }
 
   goToInfoParticipant() {
-    this.saveData();
     this.router.navigate(['/info-participant']);
   }
 
