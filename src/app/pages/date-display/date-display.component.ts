9a851be75c2c782680fb9e0c8535d6b996151194
Florian Barrier <Florian.Barrier@etu.univ-smb.fr>
2026-01-16 14:24:16 +0100

    Displayed date and updated previous/next page navigation

--- src/app/pages/date-display/date-display.component.ts
+++ src/app/pages/date-display/date-display.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-date-display',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './date-display.component.html',
   styleUrl: './date-display.component.css'
 })
 export class DateDisplayComponent {
+  currentDate: Date | null = null;
 
+  constructor(private router: Router) {
+  }
+
+  showDate() {
+    this.currentDate = new Date();
+  }
+
+  backToInfoEval() {
+    this.router.navigate(['/info-eval']);
+  }
+
+  goToInfoParticipant() {
+    this.router.navigate(['/info-participant']);
+  }
 }
